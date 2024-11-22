94ff9348f8613c6d98304281b0b0c2cbe6a88a4c
oOGutyOo <gustavo.sepulvedaq@gmail.com>
2024-11-22 20:01:48 -0300

    Cambio database 4

--- cine/settings.py
+++ cine/settings.py
@@ -75,20 +75,20 @@ WSGI_APPLICATION = 'cine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
+}  """
 
-""" DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
-} """
+}
 
 
 # Password validation
